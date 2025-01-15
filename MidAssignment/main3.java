b87ea2812991dcd08f4c80c1e3522f9a03e0c40b
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2025-01-15 07:08:56 +0100

    updates on the medain and mean

--- MidAssignment/main3.java
+++ MidAssignment/main3.java
@@ -6,12 +6,36 @@
 //      if (pop_mean == 0) {
 
 //      }
-      
+
 //      }
-    
+
 //       }
-              
+
 //   }
-      
 
+import java.util.Arrays;
 
+public class main3 {
+  public static void main(String[] args) {
+    int[] num = { 2, 5, 5, 9, 4, 7, 0, 9, 6, 11, 12 };
+
+    double Sum = 0;
+    for (int i : num) {
+      Sum += i;
+    }
+
+    double Mean = (Sum / num.length);
+
+    System.out.println("" + Mean);
+
+    Arrays.sort(num);
+
+    int Median;
+    if (num.length % 2 == 0) {
+      Median = (num[num.length - 1] / num[num.length]) / 2;
+    } else {
+      Median = (num[num.length / 2]);
+    }
+    System.out.println("" + Median);
+  }
+}
